2738c9d8fe0d523f104e35eb5bad2d3f0d309b6a
jfrrs <jfrrs@users.noreply.github.com>
2014-09-21 21:04:52 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,29 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do: you can test the funcions in this way: matriz<-makeCacheMatrix(x)    cacheSolve(matriz)
 
-## Write a short comment describing this function
+## creates a list similar to the example 
 
 makeCacheMatrix <- function(x = matrix()) {
+
+
+m <- NULL
+     set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+
+        get <- function() x
+		
+		
+	
+		
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+		
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+
 
 }
 
@@ -12,4 +32,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        
+      
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinv(m)
+        m
+
+        
 }
